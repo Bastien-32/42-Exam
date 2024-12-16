1edafac90fa4d1e845c785478f5fd0cac4836b85
Bastien Dal Lago <badal-la@c2r5p1.42perpignan.fr>
2024-12-16 16:37:27 +0100

    fix lists

--- Rank_02/lvl_4/ft_list_remove_if.c
+++ Rank_02/lvl_4/ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: badal-la <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:04:23 by badal-la          #+#    #+#             */
-/*   Updated: 2024/12/11 14:08:39 by badal-la         ###   ########.fr       */
+/*   Updated: 2024/12/16 13:39:15 by badal-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,33 @@
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	while (*begin_list)
+	if (!begin_list || !*begin_list)
+		return;
+
+	t_list	*current = *begin_list;
+	t_list	*previous = NULL;
+	
+	while (current)
 	{
-		if (cmp(begin_list, data_ref))
+		if (cmp(current->data, data_ref) == 0)
 		{
-			del(begin_list->data)
+			if (!previous)
+			{
+				*begin_list = current->next;
+				free(current);
+				current = *begin_list;
+			}
+			else
+			{
+				previous->next = current->next;
+				free(current);
+				current = previous->next;
+			}
+		}
+		else 
+		{
+			previous = current;
+			current = current->next;
 		}
 	}
 }
